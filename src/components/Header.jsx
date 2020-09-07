7c6ccccd4b73a0b95ff6ab715914e4ed7ce01029
Fjavier De Freitas <franjavikale@gmail.com>
2020-09-07 15:33:30 -0500

    conexion de datos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import '../assets/style/Header.scss';
 
 const Header = ({ children }) => (
   <header className="header">
+    <h4>IP Address Tracker</h4>
     {children}
   </header>
 );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../assets/style/Search.scss';
 
-const Search = ({ onClick, onChange }) => (
+const Search = ({ value, onClick, onChange }) => (
   <div className="search">
-    <input onChange={onChange} className="search__input" type="text" />
+    <input value={value} onChange={onChange} className="search__input" type="text" />
     <button onClick={onClick} className="search__button" type="button">
       &#x276f;
     </button>
