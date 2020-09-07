47735eb652abae17676a6a38897a528a5a35128f
Fjavier De Freitas <franjavikale@gmail.com>
2020-09-07 15:33:42 -0500

    componente mapa completo

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const Map = ({ ip }) => <div>Map {ip}</div>;
+import '../assets/style/Map.scss';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2NvcmNoZXJmamsiLCJhIjoiY2s4Nm9qOGI5MDY3ZTNkcDY1NXMyMHU4cSJ9.KZI7PHOYzUEfYb-MAgPIFQ';
+
+const Map = ({ geo }) => {
+  const mapContainerRef = useRef(null);
+
+  // initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [geo.lon, geo.lat],
+      zoom: 15.5,
+    });
+
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    // clean up on unmount
+    return () => map.remove();
+  }, [geo]);
+
+  return <div className="map" ref={mapContainerRef} />;
+};
 
 export default Map;
